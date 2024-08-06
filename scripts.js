b7b5bcafb2640e5e4a9f9d3f41aa4c6f6121e3c1
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-08-06 12:15:25 +0300

    new

---
+++ scripts.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('form');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('cpassword');
+const inputs = document.querySelectorAll('#form input');
+
+function validatePasswords() {
+    if (passwordInput.value !== confirmPasswordInput.value) {
+        confirmPasswordInput.setCustomValidity("Passwords do not match");
+    } else {
+        confirmPasswordInput.setCustomValidity(""); // Clear the custom error
+    }
+}
+submit.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if (Array.from(inputs).every((input) => input.validity.valid)) {
+        submitForm();
+    } else {
+        // alert('Please fill in all fields');
+        console.log('Please fill in all fields');
+    }
+});
+
+inputs.forEach(input =>{
+    input.addEventListener('input', () =>{
+        if (input.validity.valid) {
+            hideError(input);
+        } else{
+            showError(input);
+        }
+        if (input === passwordInput || input === confirmPasswordInput) {
+            validatePasswords();
+        }
+    })
+})
+
+function showError(input){
+    const errorEl =  input.closest('.sund-form__field').querySelector('span.error');
+    if (input.validity.valueMissing) {
+        errorEl.textContent = 'Please fill up the field';
+    } else if (input.validity.typeMismatch) {
+        errorEl.textContent = "Entered value needs to be correct type";
+        // errorEl.textContent = input.validationMessage;
+    } else if (input.validity.tooShort) {
+        errorEl.textContent = `Input should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
+    } else if(input.validity.patternMismatch){
+        errorEl.textContent = `Input should have pattern of 5 numbers;`
+    } 
+    // this else if for all validations, both inbuild and custom
+    else if(input.validationMessage){
+        errorEl.textContent = input.validationMessage;
+    }
+
+    if (input === confirmPasswordInput) {
+        // The validationMessage property of an input element reflects the message
+        // set by setCustomValidity().
+        errorEl.textContent = confirmPasswordInput.validationMessage;
+    }
+
+}
+function hideError(input){
+    const errorEl =  input.closest('.sund-form__field').querySelector('span.error');
+    errorEl.textContent = '';
+}
+function submitForm(){
+   console.log('OK');
+   document.getElementById('done').textContent = 'DONE';
+}
