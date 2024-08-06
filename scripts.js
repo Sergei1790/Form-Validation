af5fd74f20be13deb39ee6cd622035275bf56e2e
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-08-06 13:01:03 +0300

    new

--- scripts.js
+++ scripts.js
@@ -10,25 +10,35 @@ function validatePasswords() {
         confirmPasswordInput.setCustomValidity(""); // Clear the custom error
     }
 }
-submit.addEventListener('click', (e)=>{
+// submit.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     if (Array.from(inputs).every((input) => input.validity.valid)) {
+//         submitForm();
+//     } else {
+//         // alert('Please fill in all fields');
+//         console.log('Please fill in all fields');
+//     }
+// });
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (Array.from(inputs).every((input) => input.validity.valid)) {
-        submitForm();
-    } else {
-        // alert('Please fill in all fields');
+    console.log(passwordInput.value);
+    console.log(confirmPasswordInput.value);
+    if (!form.checkValidity()) {
+         // Prevent form submission if invalid
         console.log('Please fill in all fields');
+    } else {
+        submitForm(); // Call submitForm if everything is valid
     }
 });
-
 inputs.forEach(input =>{
     input.addEventListener('input', () =>{
+        if (input === passwordInput || input === confirmPasswordInput) {
+            validatePasswords();
+        }
         if (input.validity.valid) {
             hideError(input);
         } else{
             showError(input);
-        }
-        if (input === passwordInput || input === confirmPasswordInput) {
-            validatePasswords();
         }
     })
 })
@@ -46,15 +56,15 @@ function showError(input){
         errorEl.textContent = `Input should have pattern of 5 numbers;`
     } 
     // this else if for all validations, both inbuild and custom
-    else if(input.validationMessage){
+    else{
         errorEl.textContent = input.validationMessage;
     }
 
-    if (input === confirmPasswordInput) {
-        // The validationMessage property of an input element reflects the message
-        // set by setCustomValidity().
-        errorEl.textContent = confirmPasswordInput.validationMessage;
-    }
+    // if (input === confirmPasswordInput) {
+    //     // The validationMessage property of an input element reflects the message
+    //     // set by setCustomValidity().
+    //     errorEl.textContent = confirmPasswordInput.validationMessage;
+    // }
 
 }
 function hideError(input){
